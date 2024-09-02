87764959461daada610a21568835195569bd7041
nobuhiroyokota <yokota.nobuhiro.job@gmail.com>
2024-09-03 03:33:41 +0900

    値を取得すル関数の実装

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { gql, useQuery } from "@apollo/client";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const BOOKS = gql`
+    query Test {
+      test {
+        title
+        author
+      }
+    }`;
+
+  function Books() {
+
+    const { loading, error, data } = useQuery(BOOKS);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+    if (!data || !data.test) return <p>No data found</p>;
+
+    return data.test.map(({ title, author }: { title: string; author: string }) => (
+      <div key={title}>
+        <p>{author}: {title}</p>
+      </div>
+    ));
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>GraphQL Test</p>
+      <Books />
     </>
   )
 }
 
-export default App
+export default App;
